06b057e5f635ff1538c2b898b7702b774f52e2ee
Mehul Patel <mehul8@gmail.com>
2021-01-08 22:03:45 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.set('port', process.env.PORT || 8080);
 app.set('env', process.env.NODE_ENV || 'production');
 
 // App middleware
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname'));
 
 app.listen(app.get('port'), function(){
   console.log('Please go to localhost:' + app.get('port') + ' in your web browser.');
